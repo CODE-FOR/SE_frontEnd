814c304ce10adda620c8a3c4d829f56dcf80f2be
liu <meetliuzhenwei@gmail.com>
2021-04-28 22:14:18 +0800

    2021-04-28 add interpretation-card & build paper-info

--- src/view/paper/paper-info.vue
+++ src/view/paper/paper-info.vue
@@ -6,28 +6,39 @@
 
     <br />
     <div v-html="abstract"></div>
+
+    <!-- <template v-if="items.length !== 0"> -->
+      <InterpretationCard v-for="item in items" :key="item.id" v-bind="item" />
+    <!-- </template> -->
   </div>
 </template>
 
 
 <script>
+import InterpretationCard from "@/view/micro-knowledge/interpretation-card";
 import { getTags, recommend, microKnowledgeIdReq } from "@/api/microknowledge";
+import { getPaperInterpreations } from "@/api/microknowledge";
+import { getErrModalOptions, getLocalTime } from "@/libs/util.js";
 export default {
+  components: { InterpretationCard },
   data() {
     return {
-      id: this.$route.params.id,
+      id: parseInt(this.$route.params.id),
       title: "title",
       abstract: "abstract",
+      idList: [],
+      interpretations: [],
+      items: [],
     };
   },
 
   mounted() {
-    this.loadData();
+    // this.loadData()
+    this.loadInterpretation();
   },
 
   methods: {
-    loadData() {
-      alert("?");
+    loadData: function () {
       microKnowledgeIdReq(this.id, 0, "get")
         .then((res) => {
           this.title = res.data.title;
@@ -36,12 +47,58 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-      console.log(this.title);
+    },
+
+    // TODO: need backend api: use paper id to find all the interpretations about this paper
+    // res: {
+    //   interpretations: [] => {
+    //     id
+    //     content
+    //   }
+    // }
+
+    // loadInterpretation() {
+    //   getPaperInterpreations({
+    //     paper_id: id
+    //   }).then(res => {
+    //     const mapData = res.datas.interpretations.map(item => {
+    //       if (this.idList.includes(item.id)) {
+    //         return false
+    //       } else {
+    //         this.idList.push(item.id)
+    //       }
+    //       return {
+    //         id: item.id,
+    //         content: item.content
+    //       }
+    //     })
+    //     this.interpretations.push(...mapData.filter(x => x))
+    //   })
+    // }
+    loadInterpretation: function () {
+      const mapData = {
+        id: 1,
+        creator: {
+          id: 1,
+          username: "lzw_user",
+        },
+        kind: 1,
+        createAt: getLocalTime("August 19, 1975 23:15:30 GMT+00:00"),
+        publishedYear: 2021,
+        content: "<p>test</p>",
+        tags: ["a", "b", "c", "d"],
+        isLike: 0,
+        isCollect: 0,
+        likeNumber: 0,
+        favorNumber: 0,
+        displayType: 0,
+        source: "http://www.baidu.com",
+        citation: "http://www.baidu.com",
+        evidences: [],
+      };
+      this.items.push(mapData);
+      console.log(items)
     },
   },
-
-  loadInterpretation() {
-
-  }
 };
 </script>
