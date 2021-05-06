a47904e771cc059a7851e53b1a74338088503db4
liu <meetliuzhenwei@gmail.com>
2021-05-06 22:55:49 +0800

    fix home api spelling

--- src/api/microknowledge.js
+++ src/api/microknowledge.js
@@ -88,12 +88,12 @@ export const microKnowledgeIdReq = (id, type, method, params) => {
 }
 
 export const getPaperList = (pageIndex, params) => {
-  const url = `api/paper/page/${parseInt(pageIndex)}`
+  const url = `/api/paper/page/${parseInt(pageIndex)}`
   return getRequest(url, 'get', params)
 }
 
 export const recommend = (params) => {
-  const url = 'api/recommend'
+  const url = '/api/recommend'
   return getRequest(url, 'get', params)
 }
 
