1b41d7e80d2ea74eac51e8a7351c087d184fc151
liu <meetliuzhenwei@gmail.com>
2021-05-28 22:24:36 +0800

    fix get icon by id url

--- src/api/user.js
+++ src/api/user.js
@@ -105,7 +105,7 @@ export const getIcon = (params) => {
 }
 
 export const getIconById = (params) => {
-  const url =  `/api/user/icon_by_id`
+  const url =  `/api/user/icon-by-id`
   return getRequest(url, 'get', params)
 }
 
